670f0f45c43f9b650119375368c16e4206760303
Noha Omar <93240949+NohaOmar20@users.noreply.github.com>
2023-12-28 10:06:52 +0200

    Update search_results.php

--- search_results.php
+++ search_results.php
@@ -1,40 +1,34 @@
 <?php
+// Connect to the database
+include 'dp.php';
+
 // Check if the search button is clicked
-if(isset($_POST['search'])) {
+if(isset($_GET['search'])) {
     // Get the search query from the user
-    $search_query = $_POST['search_query'];
-    
-    // Connect to the database
-    $conn = mysqli_connect("localhost", "username", "password", "database_name");
-    
+    $search_query = $_GET['search'];
+
     // Check if the connection is successful
-    if(!$conn) {
+    if(!$con) {
         die("Connection failed: " . mysqli_connect_error());
     }
-    
+
     // Construct the SQL query
-    $sql = "SELECT * FROM products WHERE product_name LIKE '%$search_query%'";
-    
+    $sql = "SELECT * FROM products WHERE name LIKE '%$search_query%'";
+
     // Execute the query
-    $result = mysqli_query($conn, $sql);
-    
-    // Check if there are any results
+    $result = mysqli_query($con, $sql);
+
+    // Check if there are any results 
     if(mysqli_num_rows($result) > 0) {
         // Display the results
         while($row = mysqli_fetch_assoc($result)) {
-            echo "<p>" . $row['product_name'] . "</p>";
+            echo "<p>" . $row['name'] . "</p>";
         }
     } else {
         echo "No results found.";
     }
-    
+
     // Close the database connection
-    mysqli_close($conn);
+    mysqli_close($con);
 }
 ?>
-
-<!-- HTML form for the search input -->
-<form method="post" action="search_results.php">
-    <input type="text" name="search_query" placeholder="Search for products...">
-    <button type="submit" name="search">Search</button>
-</form>

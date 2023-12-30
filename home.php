8d0c84a117e4360d60a65d4072826c6e55d4fe89
Noha Omar <93240949+NohaOmar20@users.noreply.github.com>
2023-12-30 16:08:36 +0200

    Update home.php

--- home.php
+++ home.php
@@ -43,9 +43,7 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
         <!--==Using-Font-Awesome=====================-->
         <script src="https://kit.fontawesome.com/5471644867.js" crossorigin="anonymous"></script>
-    
-        <script src = 'Home Page JS.js' defer></script>
-            
+                
        <link rel="shortcut icon" type="image/jpg" href="C:\Users\hp\Desktop\College\First Semester\Introduction To Web Technologies\Notepad ++ Files\Project\favicon.ico"/>
     
         </head>
@@ -85,24 +83,16 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
                     <li>
     
-                        <a href="#category">
+                        <a href="products.php">
     
-                            Categories
+                            Products
                         
                         </a>
                     
                     </li>
-                     <li>
-                        
-                        <a href="products.php">
-                            
-                            products
-                        
-                        </a>
-                    <li>
 
     
-                  
+                    <li>
                     
                     </li>
     
@@ -143,13 +133,8 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <a href="index.php?page=cart" class="cart">
     
                         <i class="fas fa-shopping-cart"></i>
-                        <img src="https://cdn-icons-png.flaticon.com/512/1413/1413908.png" style="width:30px;height:30px;">
+                        <img src="imgs/cart.png" style="width:30px;height:30px;">
     
-                        <span>
-                            
-                            2
-                        
-                        </span>
     
                     </a>
     
@@ -167,121 +152,100 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
     
                 <img alt="bg" class="bg-1_rev" src="v.png" >
     
+    
                 <!--text------->
                 <div class="search-banner-text">
-                    <h1>Order Your Groceries,All you need </h1>
     
-
-                <form class="search-box" method="post" action="search_results.html">
-
-                    <!--icon------>
-                    <i class="fas fa-search"></i>
-
-                    <!--input----->
-                    <input type="text" class="search-input" placeholder="Search" name="search" size="80px" required>
-
-                    <!--btn------->
-                    <input name="submit" type="submit" class="search-btn" value="Search">
-
-                </form>
+                    <h1>
+                        
+                        Order Your Groceries,All you need
+                    
+                    </h1>
+    
+    
+                    <!--search-box------>
+                    <form action="search_results.php" class="search-box" >
+    
+                        <!--icon------>
+                        <i class="fas fa-search"></i>
+    
+                        <!--input----->
+                        <input type="text" class="search-input" placeholder="Search" name="search" size = "80px" required>
+    
+                        <!--btn------->
+                        <input type="submit" class="search-btn" value="Search">
+    
+                    </form>
     
                     <br>
     
-                    <h3>
     
-                        <a href = 'Best Deals HTML.html' style = 'color: #40aa54'>
+                </div>
     
-                            Click here to learn about today's best deals
-    
-                        </a>
-    
-                    </h3>
-
+            </section>
     
             <!--search-banner-end--------------->
     
-            <h2 class = 'sale-heading'>
+        <h2 class = 'sale-heading'>
     
-                Republic Day Sale is Live For
+                Flash sale started In 
     
             </h2>
     
             <div class="countdown-container">
     
-                <div class="countdown-el days-c">
-                    <p class="big-text" id="days">0</p>
-                    <span>Days</span>
-                </div>
-                <div class="countdown-el hours-c">
-                    <p class="big-text" id="hours">0</p>
-                    <span>Hours</span>
-                </div>
-                <div class="countdown-el mins-c">
-                    <p class="big-text" id="mins">0</p>
-                    <span>Min</span>
-                </div>
-                <div class="countdown-el seconds-c">
-                    <p class="big-text" id="seconds">0</p>
-                    <span>Seconds</span>
-                </div>
+                <p class="big-text" id="countdown"></p>
+
+                <script>
+                // Set the date we're counting down to
+                // Get today's date
+                var now = new Date();
+                var dayOfWeek = now.getDay();
+
+                // Set the date we're counting down to
+                var countDownDate = new Date();
+
+                // If it's not Thursday or Friday, set countdown to the next Thursday
+                if (dayOfWeek != 4 && dayOfWeek != 5) {
+                    countDownDate.setDate(now.getDate() + ((4 + 7 - dayOfWeek) % 7));
+                }
+
+                // Set the countdown time
+                countDownDate.setHours(23, 59, 59);
+
+                // Update the count down every 1 second
+                var countdownfunction = setInterval(function() {
+
+                    // Get today's date and time
+                    now = new Date().getTime();
+
+                    // Find the distance between now and the count down date
+                    var distance = countDownDate - now;
+
+                    // Time calculations for days, hours, minutes and seconds
+                    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+                    // Display the result in the element with id="countdown"
+                    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+                    // If the count down is finished, write some text 
+                    if (distance < 0) {
+                        clearInterval(countdownfunction);
+                        document.getElementById("countdown").innerHTML = "EXPIRED";
+                    }
+				}, 1000);
+                </script>
+
+                    
             </div>
-            <!--==category=========================================-->
-            <section id="category">
-    
-                <!--heading---------------->
-                <div class="category-heading">
-    
-                    <h2>
-                        
-                        Category
-                    
-                    </h2>
-    
-                    <span>
-                        
-                        All
-                    
-                    </span>
-    
-                </div>
-    
-                <!--box-container---------->
-                <div class="category-container">
-    
-                    <!--box---------------->
-                    <a class="category-box" href = 'Fruits Category Page HTML.html'>
-                        <img alt="Fruits and Vegetables" src="https://p1.hiclipart.com/preview/593/493/164/fruits-vegetable-logo-fruits-vegetable-fruit-vegetable-food-nut-bowl-png-clipart.jpg">
-                        <span>Fruits & <br> Vegetables</span>
-                    </a>
-                    <!--box---------------->
-                    <a class="category-box" href = 'Chicken Category Page HTML.html'>
-                        <img alt="Chicken" src="https://banner2.cleanpng.com/20180528/lro/kisspng-roast-chicken-fried-chicken-barbecue-chicken-hoho-5b0c72bb14ac07.7537007015275424590847.jpg">
-                        <span>Chicken</span>
-                    </a>
-                    <!--box---------------->
-                    <a class="category-box" href="Meat Category Page HTML.html">
-                        <img alt="Meat" src="https://i.pinimg.com/originals/10/e0/56/10e056e59cd128c9eeeeccf0327c6b39.png">
-                        <span>Meat</span>
-                    </a>
-                    <!--box---------------->
-                    <a class="category-box" href = 'Fish Category Page HTML.html'>
-                        <img alt="Fish" src="https://image.similarpng.com/very-thumbnail/2022/01/Fish-logo-template-on-transparent-background-PNG.png">
-                        <span>Fish</span>
-                    </a>
-                    <!--box---------------->
-                    <a class="category-box" href = 'Cheese Category Page HTML.html'>
-                        <img alt="Cheese" src="https://i.etsystatic.com/26743355/r/il/5bbd59/2981450570/il_fullxfull.2981450570_eeck.jpg">
-                        <span>Cheese</span>
-                    </a>
-                </div>
-                
-            </section>
-            <!--category-end----------------------------------->
             <!--==Products====================================-->
             <section id="popular-product">
             <!-- Heading -->
             <div class="product-heading">
-                <h3>Explore Popular Products</h3>
+                <h3>Explore New Products</h3>
                 <span>View All</span>
             </div>
             <!-- Product Container -->
@@ -305,146 +269,21 @@ $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
         </section>
 
 
-                </div>
-            </section>
-            <!--popular-product-end--------------------->
-            <!--Popular-bundle-pack===================================-->
-            <section id="popular-bundle-pack">
-                <!--heading-------------->
-                <div class="product-heading">
-                    <h3>Popular Bundle Pack</h3>
-                </div>
-                <!--box-container------>
-                <div class="product-container">
-                    <!--box---------->
-                    <div class="product-box">
-                        <img alt="pack" src="https://i.imgur.com/Zm8Xo2j.png">
-                        <strong>Big Pack</strong>
-                        <span class="quantity">Lemon, Tomato, Potato,+4</span>
-                        <span class="price">LE. 500</span>
-                        <!--cart-btn------->
-                        <a href="Shopping Cart HTML.html" class="cart-btn">
-                            <i class="fas fa-shopping-bag"></i> Add to Cart
-                        </a>
-                        <!--like-btn------->
-                        <a class="like-btn">
-                            <i class="far fa-heart"></i>
-                        </a>
-                    </div>
-                    <!--box---------->
-                    <div class="product-box">
-                        <img alt="apple" src="https://i.imgur.com/vMA9mRm.jpg">
-                        <strong>Large Pack</strong>
-                        <span class="quantity">Lemon, Tomato, Potato,+2</span>
-                        <span class="price">LE. 800</span>
-                        <!--cart-btn------->
-                        <a href="Shopping Cart HTML.html" class="cart-btn">
-                            <i class="fas fa-shopping-bag"></i> Add to Cart
-                        </a>
-                        <!--like-btn------->
-                        <a class="like-btn">
-                            <i class="far fa-heart"></i>
-                        </a>
-                    </div>
-                    <!--box---------->
-                    <div class="product-box">
-                        <img alt="apple" src="https://i.imgur.com/CeVqxe2.png">
-                        <strong>Small Pack</strong>
-                        <span class="quantity">Lemon, Tomato, Potato</span>
-                        <span class="price">LE. 300</span>
-                        <!--cart-btn------->
-                        <a href="Shopping Cart HTML.html" class="cart-btn">
-                            <i class="fas fa-shopping-bag"></i> Add to Cart
-                        </a>
-                        <!--like-btn------->
-                        <a class="like-btn">
-                            <i class="far fa-heart"></i>
-                        </a>
-                    </div>
-                    <!--box---------->
-                    <div class="product-box">
-                        <img alt="pack" src="https://i.imgur.com/Zm8Xo2j.png">
-                        <strong>Big Pack</strong>
-                        <span class="quantity">Lemon, Tomato, Potato,+4</span>
-                        <span class="price">LE. 900</span>
-                        <!--cart-btn------->
-                        <a href="Shopping Cart HTML.html" class="cart-btn">
-                            <i class="fas fa-shopping-bag"></i> Add to Cart
-                        </a>
-                        <!--like-btn------->
-                        <a class="like-btn">
-                            <i class="far fa-heart"></i>
-                        </a>
-                    </div>
-                    <!--box---------->
-                    <div class="product-box">
-                        <img alt="apple" src="https://i.imgur.com/vMA9mRm.jpg">
-                        <strong>Large Pack</strong>
-                        <span class="quantity">Lemon, Tomato, Potato,+2</span>
-                        <span class="price">LE. 700</span>
-                        <!--cart-btn------->
-                        <a href="Shopping Cart HTML.html" class="cart-btn">Add to Cart
-                       
-  
-                        </a>
-                        
-                        <!--like-btn------->
-                        <a class="like-btn">
-                            <i class="far fa-heart"></i>
-                        </a>
-                    </div>
-                    <!--box---------->
-                    <div class="product-box">
-                        <img alt="apple" src="https://i.imgur.com/CeVqxe2.png">
-                        <strong>Small Pack</strong>
-                        <span class="quantity">Lemon, Tomato, Potato</span>
-                        <span class="price">Rs. 400</span>
-                        <!--cart-btn------->
-                        <a href="Shopping Cart HTML.html" class="cart-btn">
-                            <i class="fas fa-shopping-bag"></i> Add to Cart
-                        </a>
-                        <!--like-btn------->
-                        <a class="like-btn">
-                            <i class="far fa-heart"></i>
-                        </a>
-                    </div>
-                </div>
-            </section>
-            <!--popular-bundle-pack-end-------------------------------->
+                
             <!--==Footer=============================================-->
             <footer>
                 <div class="footer-container">
                     <!--logo-container------>
                     <div class="footer-logo">
                         <a href="file:///C:/Users/RAGHAV/Desktop/Coding/Grocery/Logo.png"><span>RO</span>NO</a>
-                    </div>
-                    <!--links------------------------->
-                <div class="footer-links">
-                    <strong>Product</strong>
-                    <ul>
-                        <li><a href="#">Grocery</a></li>
-                        <li><a href="#">Popular</a></li>
-                        <li><a href="#">New</a></li>
-                    </ul>
-                </div>
-                <!--links------------------------->
-                <div class="footer-links">
-                    <strong>Category</strong>
-                    <ul>
-                        <li><a href="#">Chicken</a></li>
-                        <li><a href="#">Vegetables and fruits</a></li>
-                        <li><a href="#">Meat</a></li>
-                        <li><a href="#">Fish</a></li>
-                        <li><a href="#">Cheese</a></li>
-                    </ul>
-                </div>
+                  
                 <!--links-------------------------->
                 <div class="footer-links">
                     <strong>Contact</strong>
                     <ul>
                         <li><a href="#">Email : RONOgrocery@gmail.com</a></li>
                     </ul>
-                    <br><p style="color: rgb(112, 134, 153);">Copyright ©2022 | All Rights Reserved</p>
+                    <br><p style="color: rgb(112, 134, 153);">Copyright ©2023 | All Rights Reserved</p>
                 </div>
                 </div>
             </footer>
